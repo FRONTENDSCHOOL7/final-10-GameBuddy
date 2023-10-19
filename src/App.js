3034b10b980025420f08ce8b02ab25946bebeb2f
GedFlow <ptj1327@naver.com>
2023-10-19 15:36:43 +0900

    [Feat]-메인페이지, 로그인페이지, 회원가입 페이지 틀 작성. 로그인 기능 구현(로컬 스토리지에 토큰을 저장하는 방식), 메인페이지에서 토큰 없으면 로그인 페이지로 이동. 회원가입 기능 미구현, 토큰 유효성 검사 기능 구현 필요

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Login from './Components/Login/Login';
+import SignUp from './Components/SignUp/SignUp';
+
 function App() {
   return (
-    <div>
-      hello world
+    <div className="App">
+      <Routes>
+        <Route path='/' element={localStorage.getItem("token") ? <div>메인페이지</div> : <Navigate to="/login" /> } />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        
+      </Routes>
     </div>
   );
 }
