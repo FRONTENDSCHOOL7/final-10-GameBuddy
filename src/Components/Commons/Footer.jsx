1927c6b6a4e84f54a976b9fdcc458e7cca4c3b58
Hajin-Bang <hajin0619@naver.com>
2023-10-20 17:49:49 +0900

    [Feat]-footer icon 변경 및 활성화 구현

--- src/Components/Commons/Footer.jsx
+++ src/Components/Commons/Footer.jsx
@@ -1,34 +1,87 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 // import { useNavigate } from "react-router-dom";
-import { ReactComponent as HomeIcon } from "../../assets/image/icon-home.svg";
-import { ReactComponent as ChatIcon } from "../../assets/image/icon-message-circle.svg";
-import { ReactComponent as WriteIcon } from "../../assets/image/icon-write.svg";
-import { ReactComponent as ProfileIcon } from "../../assets/image/icon-user.svg";
+import { ReactComponent as HomeIcon } from "../../assets/image/HomeIcon.svg";
+import { ReactComponent as ChatIcon } from "../../assets/image/ChatIcon.svg";
+import { ReactComponent as WriteIcon } from "../../assets/image/WriteIcon.svg";
+import { ReactComponent as ProfileIcon } from "../../assets/image/ProfileIcon.svg";
 
 function Footer() {
+  const [active, setActive] = useState("home");
   // const navigate = useNavigate();
 
   const menus = [
-    { name: "홈", icon: HomeIcon, path: "/main", id: "home" },
-    { name: "채팅", icon: ChatIcon, path: "/chat", id: "chat" },
-    { name: "게시물 작성", icon: WriteIcon, path: "/write", id: "write" },
-    { name: "프로필", icon: ProfileIcon, path: "/profile", id: "profile" }
+    { name: "홈", icon: StyledHomeIcon, path: "/main", id: "home" },
+    { name: "채팅", icon: StyledChatIcon, path: "/chat", id: "chat" },
+    { name: "게시물 작성", icon: StyledWriteIcon, path: "/write", id: "write" },
+    { name: "프로필", icon: StyledProfileIcon, path: "/profile", id: "profile" }
   ];
 
   return (
     <FooterContainer>
-      {menus.map((menu) => (
-        <Item
-          key={menu.id}
-          onClick={() => {
-            // navigate(menu.path);
-          }}>
-          <menu.icon />
-          <span>{menu.name}</span>
-        </Item>
-      ))}
+      {menus.map((menu) => {
+        const Icon = menu.icon;
+        return (
+          <Item
+            key={menu.id}
+            onClick={() => setActive(menu.id)}
+            active={active === menu.id}>
+            <Icon active={active === menu.id} />
+            <span>{menu.name}</span>
+          </Item>
+        );
+      })}
     </FooterContainer>
   );
 }
 
 export default Footer;
+
+const FooterContainer = styled.footer`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: white;
+`;
+
+const Item = styled.button`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: none;
+  border: none;
+  cursor: pointer;
+  color: ${({ active }) => (active ? "#5865F2" : "#A4A4A4")};
+`;
+
+const StyledHomeIcon = styled(HomeIcon)`
+  path {
+    stroke: ${({ active }) => (active ? "#5865F2" : "#A4A4A4")};
+    /* fill: ${({ active }) => (active ? "#5865F2" : "none")}; */
+  }
+`;
+
+const StyledChatIcon = styled(ChatIcon)`
+  path {
+    stroke: ${({ active }) => (active ? "#5865F2" : "#A4A4A4")};
+    /* fill: ${({ active }) => (active ? "#5865F2" : "none")}; */
+  }
+`;
+
+const StyledWriteIcon = styled(WriteIcon)`
+  path {
+    stroke: ${({ active }) => (active ? "#5865F2" : "#A4A4A4")};
+    /* fill: ${({ active }) => (active ? "#5865F2" : "none")}; */
+  }
+  rect {
+    stroke: ${({ active }) => (active ? "#5865F2" : "#A4A4A4")};
+    /* fill: ${({ active }) => (active ? "#5865F2" : "none")}; */
+  }
+`;
+
+const StyledProfileIcon = styled(ProfileIcon)`
+  path {
+    stroke: ${({ active }) => (active ? "#5865F2" : "#A4A4A4")};
+    /* fill: ${({ active }) => (active ? "#5865F2" : "none")}; */
+  }
+`;
