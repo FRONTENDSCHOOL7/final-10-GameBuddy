b61e96b2215350abce255c8a310354cdedb14692
Jun Hong Park <hongi97@naver.com>
2023-10-20 22:02:24 +0900

    [Feat]-MainFeed구성 및 followingList API 데이터  호출

---
+++ src/API/followingPostAPI.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+async function followingPostAPI() {
+    try {
+        const token = localStorage.getItem("token");
+        let result = await axios({
+            method: 'get',
+            url: `https://api.mandarin.weniv.co.kr/post/feed`,
+            headers: {
+                "Authorization": `Bearer ${token}`
+            },
+        })
+        const post = await result.data.posts
+        return post
+    } catch (error) {
+        console.log("PostList Error", error)
+    }
+};
+
+export default followingPostAPI;
--- src/Components/Main/PostList.jsx
+++ src/Components/Main/PostList.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import followingPostAPI from "../../API/followingPostAPI";
+import { useRecoilState } from "recoil";
+import { postListDataAtom } from "../../Store/Store";
 
 function PostList() {
-  return <div>PostList</div>;
+  const [postData, setPostData] = useRecoilState(postListDataAtom);
+
+  useEffect(() => {
+    async function fetchData() {
+      const obj = await followingPostAPI();
+      setPostData(obj);
+    }
+    fetchData();
+  }, []);
+
+  return postData.length === 0 ? (
+    <div>비어있음</div>
+  ) : (
+    <div>
+      {postData.map((post) => {
+        return (
+          <div key={post.id}>
+            <div>{post.id}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default PostList;
