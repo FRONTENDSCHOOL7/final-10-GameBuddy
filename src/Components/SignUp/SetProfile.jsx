8469ab9fbd89ee9607a3c6a065b5b2359919d88b
GedFlow <ptj1327@naver.com>
2023-10-22 01:03:14 +0900

    [Style]-회원가입 화면(유저 프로필 입력화면) UI 개발. 사용자 이름의 경우 아무것도 입력안하면 이메일을 바탕으로 임의의 값으로 설정됨. 에러를 화면상에 띄우기 위해 signUpAPI.js 코드 일부 수정(리턴값만 조금 수정함)

--- src/Components/SignUp/SetProfile.jsx
+++ src/Components/SignUp/SetProfile.jsx
@@ -1,12 +1,73 @@
 import React, {useState, useEffect} from 'react'
 import { useNavigate } from 'react-router-dom';
+import styled from "styled-components";
 import DefaultImage from '../../assets/image/char_inactive.png';
 import signUpAPI from '../../API/signUpAPI';
+
+const Container = styled.div`
+  background-color: #ffffff;
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+  position:relative;
+  font-size:12px;
+`
+const SetProfileForm = styled.form`
+  margin: 0 auto;
+  width: 86%;
+  position: relative;
+  top:20%;
+  color: #767676;
+`
+const ProfileSettingLogo = styled.p`
+  margin-bottom: 36px;
+  font-size: 25px;
+  font-weight:bold;
+  color: #000000;
+`
+const PTag = styled.p`
+    margin:14px 0 6px 0;
+    text-align:left;
+`
+const InputTag = styled.input`
+  width: 100%;
+  height: 23px;
+  border-width: 0 0 1px;
+  padding: 0 0 1px 0;
+  font-size: 20px;
+  &:focus {
+    outline: none;
+    border-color: #DBDBDB;
+  }
+  &::placeholder {
+    color: #DBDBDB;
+    font-size: 14px;
+  }
+`
+const SubmitBtn = styled.button`
+width: 100%;
+height: 44px;
+margin-top: 30px;
+border-radius: 44px;
+border: none;
+background-color: ${props => props.disabled ? '#8EA1E1' : '#5865f2'};
+/* background-color: #8EA1E1; */
+/* background-color: #5865f2; */
+color: #ffffff;
+`
+const Warning = styled.p`
+  color: #ff0000;
+  text-align:left;
+  font-size: 12px;
+  margin-top: 4px;
+`
+
 
 function SetProfile({email, password}) {
   const [userName, setUserName] = useState('');
   const [accountName, setAccountName] = useState('');
   const [intro, setIntro] = useState('');
+  const [isSignUp, setIsSignUp] = useState(0);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -17,20 +78,26 @@ function SetProfile({email, password}) {
   const onSubmitHandler = async (e) => {
     e.preventDefault();
     const image = '';
-    const signupResult = await signUpAPI(userName, email, password, accountName, intro, image);
+    if(userName.length < 2 || userName.length > 10) {
+      setIsSignUp(2)
+    } else {
+      const signupResult = await signUpAPI(userName, email, password, accountName, intro, image);
 
-    if(signupResult) {
-      alert('회원가입 성공했습니다! 가입한 이메일과 비밀번호로 로그인해주세요!')
-      navigate("/login")
-    } else alert("회원가입에 실패했습니다!")
+      if(signupResult === 0) {
+        alert('회원가입 성공했습니다! 가입한 이메일과 비밀번호로 로그인해주세요!')
+        navigate("/login")
+      } else if(signupResult === 1) setIsSignUp(1)
+    }
   };
 
   const onChangeUserName = (e) => {
     setUserName(e.target.value);
+    if(isSignUp === 2) setIsSignUp(0);
   };
 
   const onChangeAccountName = (e) => {
     setAccountName(e.target.value);
+    if(isSignUp === 1) setIsSignUp(0);
   };
 
   const onChangeIntro = (e) => {
@@ -38,20 +105,25 @@ function SetProfile({email, password}) {
   };
 
   return (
-    <>
-      <form onSubmit={onSubmitHandler}>
-        <h1>프로필 설정</h1>
+    <Container>
+      <SetProfileForm onSubmit={onSubmitHandler}>
+        <ProfileSettingLogo>프로필 설정</ProfileSettingLogo>
+
         <img src={DefaultImage} alt="디폴트이미지" />
-        사용자이름<input type="text" onChange={onChangeUserName} />
-        <br />
-        계정ID<input type="text" onChange={onChangeAccountName} />
-        <br />
-        소개<input type="text" onChange={onChangeIntro} />
-        <br />
-        <br />
-        <button type='submit'>Game Buddy 시작하기!</button>
-      </form>
-    </>
+
+        <PTag>사용자이름</PTag>
+        <InputTag type="text" onChange={onChangeUserName} placeholder='2~10자 이내여야 합니다.' />
+        <Warning style={isSignUp === 2 ? {display:"block"} : {display:"none"}}>*유저이름은 2~10자 이내여야 합니다.</Warning>
+        
+        <PTag>계정ID</PTag>
+        <InputTag type="text" onChange={onChangeAccountName} placeholder='영문, 숫자, 마침표, 밑줄만 사용 가능합니다. ' />
+        <Warning style={isSignUp === 1 ? {display:"block"} : {display:"none"}}>*영문, 숫자, 밑줄 및 마침표만 사용할 수 있습니다.</Warning>
+        
+        <PTag>소개</PTag>
+        <InputTag type="text" onChange={onChangeIntro} placeholder='자신을 소개해주세요!' />
+        <SubmitBtn type='submit'>Game Buddy 시작하기!</SubmitBtn>
+      </SetProfileForm>
+    </Container>
   )
 }
 
--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -3,22 +3,20 @@ import styled from "styled-components";
 import emailValidAPI from "../../API/emailValidAPI";
 
 const Container = styled.div`
-    background-color: #ffffff;
-    width: 100%;
-    height: 100vh;
-    text-align: center;
-    position:relative;
-    font-size:12px;
-  `
-
-const SignUpForm = styled.form`
-margin: 0 auto;
-width: 86%;
-position: relative;
-top:20%;
-color: #767676;
+  background-color: #ffffff;
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+  position:relative;
+  font-size:12px;
 `
-
+const SignUpForm = styled.form`
+  margin: 0 auto;
+  width: 86%;
+  position: relative;
+  top:20%;
+  color: #767676;
+`
 const SignUpLogo = styled.p`
   margin-bottom: 36px;
   font-size: 25px;
@@ -88,23 +86,21 @@ function SignUp({setCheck, email, setEmail, password, setPassword}) {
   };
 
   return (
-    <>
-      <Container>
-        <SignUpForm action="" onSubmit={onSubmitHandler}>
-          <SignUpLogo>이메일로 회원가입</SignUpLogo>
-          <PTag>이메일</PTag>
-          <InputTag type="text" onChange={onChangeEmail} placeholder="이메일 주소를 입력해 주세요" />
-          <Warning style={emailValidResult === 1 ? {display:"block"} : {display:"none"}}>*이미 가입된 이메일 입니다.</Warning>
-          <Warning style={emailValidResult === 2 ? {display:"block"} : {display:"none"}}>*잘못된 이메일 형식입니다.</Warning>
-          <br />
-          <PTag>비번</PTag>
-          <InputTag type="password" onChange={onChangePassword} placeholder="6자리 이상의 비밀번호를 입력해 주세요"/>
-          <br />
-          <br />
-          <SubmitBtn type="submit" disabled={!email || password.length < 6}>다음</SubmitBtn>
-        </SignUpForm>
-      </Container>
-    </>
+    <Container>
+      <SignUpForm action="" onSubmit={onSubmitHandler}>
+        <SignUpLogo>이메일로 회원가입</SignUpLogo>
+        <PTag>이메일</PTag>
+        <InputTag type="text" onChange={onChangeEmail} placeholder="이메일 주소를 입력해 주세요" />
+        <Warning style={emailValidResult === 1 ? {display:"block"} : {display:"none"}}>*이미 가입된 이메일 입니다.</Warning>
+        <Warning style={emailValidResult === 2 ? {display:"block"} : {display:"none"}}>*잘못된 이메일 형식입니다.</Warning>
+        <br />
+        <PTag>비번</PTag>
+        <InputTag type="password" onChange={onChangePassword} placeholder="6자리 이상의 비밀번호를 입력해 주세요"/>
+        <br />
+        <br />
+        <SubmitBtn type="submit" disabled={!email || password.length < 6}>다음</SubmitBtn>
+      </SignUpForm>
+    </Container>
   );
 }
 
