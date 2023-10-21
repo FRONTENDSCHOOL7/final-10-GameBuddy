28fd7d733709bc0bed028f44fca32d9de773fb97
Hajin-Bang <hajin0619@naver.com>
2023-10-21 21:35:31 +0900

    [Style]-헤더 디자인 수정

--- src/Components/Commons/Header/HeaderStyle.jsx
+++ src/Components/Commons/Header/HeaderStyle.jsx
@@ -8,3 +8,30 @@ export const HeaderContainer = styled.div`
   background-color: #ffff;
   border-bottom: 1px solid #dbdbdb;
 `;
+
+export const Logo = styled.h1`
+  font-family: var(--Black-ops);
+  font-size: 22px;
+  color: var(--color-purple);
+`;
+
+export const FollowersTitle = styled.div`
+  font-family: var(--Roboto-B);
+  font-size: 22px;
+  color: var(--color-purple);
+`;
+
+export const StyledIconButton = styled.button`
+  border: none;
+  background: none;
+  cursor: pointer;
+  padding: 5px;
+`;
+
+export const Input = styled.input`
+  flex: 1;
+  padding: 8px;
+  background-color: #f2f2f2;
+  border: 1px solid #f2f2f2;
+  border-radius: 32px;
+`;
