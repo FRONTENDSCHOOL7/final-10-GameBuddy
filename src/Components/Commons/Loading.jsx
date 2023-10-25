9d93864855fa7766d2340c93e71aa103d27339a3
Hajin-Bang <hajin0619@naver.com>
2023-10-26 06:44:35 +0900

    [Feat]-loadingPage디자인 수정 및 mainFeed에 적용

--- src/Components/Commons/Loading.jsx
+++ src/Components/Commons/Loading.jsx
@@ -1,6 +1,52 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import LoadingGIF from "../../assets/image/Loading.gif";
+import MainChar from "../../assets/image/main_char.png";
+import MainCharFace from "../../assets/image/main_char_face.svg";
+import MainCharHead from "../../assets/image/main_char_head.svg";
+import MainCharPoint from "../../assets/image/main_char_point.svg";
+
+// 헤드폰에 대한 애니메이션 (현재 주석처리(사용x))
+const rubberBand = keyframes`
+  from, to {
+    transform: scale(1);
+  }
+  30% {
+    transform: scaleX(1.25);
+  }
+  40% {
+    transform: scaleX(0.75);
+  }
+  50% {
+    transform: scaleX(1.15);
+  }
+  65% {
+    transform: scaleX(0.95);
+  }
+  75% {
+    transform: scaleX(1.05);
+  }
+`;
+
+// loading text에 대한 애니메이션 (현재 주석처리(사용x))
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+// pointImage에 대한 애니메이션 (사용중)
+const scaleAnimation = keyframes`
+  0%, 100% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(2);
+  }
+`;
 
 export const LoadingContainer = styled.div`
   position: absolute;
@@ -8,7 +54,6 @@ export const LoadingContainer = styled.div`
   height: 100vh;
   top: 0;
   left: 0;
-  background: #ffffffb7;
   color: #8ea1e1;
   z-index: 999;
   display: flex;
@@ -17,17 +62,97 @@ export const LoadingContainer = styled.div`
   justify-content: center;
 `;
 
+export const CharacterContainer = styled.div`
+  position: relative;
+  width: 20vmin;
+  max-width: 300px;
+`;
+
+export const FaceImage = styled.img`
+  position: absolute;
+  top: -18vmin;
+  left: 1vmin;
+  width: 15vmin;
+  max-width: 250px;
+`;
+
+export const HeadImage = styled.img`
+  position: absolute;
+  top: -26vmin;
+  left: -10vmin;
+  width: 36vmin;
+  max-width: 400px;
+  opacity: 1;
+  /* animation: ${rubberBand} 0.1s 0.05s alternate; */
+`;
+
+export const PointImage = styled.img`
+  position: absolute;
+  top: -21vmin;
+  left: 6vmin;
+  width: 3vmin;
+  max-width: 50px;
+  animation: ${scaleAnimation} 0.5s alternate;
+`;
+
 export const LoadingText = styled.div`
   font: 1rem "Noto Sans KR";
   text-align: center;
-  font-size: 20px;
+  font-size: 5vmin;
+
+  span {
+    display: inline-block;
+    opacity: 1;
+    /* animation: ${fadeIn} 0.125s forwards, ${fadeIn} 0.125s forwards 1.55s; */
+
+    &:nth-child(1) {
+      animation-delay: 0.075s, 1.65s;
+    } // L
+    &:nth-child(2) {
+      animation-delay: 0.15s, 1.725s;
+    } // o
+    &:nth-child(3) {
+      animation-delay: 0.225s, 1.8s;
+    } // a
+    &:nth-child(4) {
+      animation-delay: 0.3s, 1.875s;
+    } // d
+    &:nth-child(5) {
+      animation-delay: 0.375s, 1.95s;
+    } // i
+    &:nth-child(6) {
+      animation-delay: 0.45s, 2.025s;
+    } // n
+    &:nth-child(7) {
+      animation-delay: 0.525s, 2.1s;
+    } // g
+    &:nth-child(9) {
+      animation-delay: 0.6s, 2.175s;
+    } // space
+    &:nth-child(11) {
+      animation-delay: 0.675s, 2.25s;
+    }
+    &:nth-child(13) {
+      animation-delay: 0.75s, 2.325s;
+    }
+  }
 `;
 
 export default function Loading() {
+  const fullText = "Loading . . .";
+
   return (
     <LoadingContainer>
-      <img src={LoadingGIF} alt="로딩중" width="18%" />
-      <LoadingText>Loading . . .</LoadingText>
+      <CharacterContainer>
+        <FaceImage src={MainCharFace} alt="메인 캐릭터 얼굴" />
+        <HeadImage src={MainCharHead} alt="메인 캐릭터 헤드폰" />
+        <PointImage src={MainCharPoint} alt="메인 캐릭터 불빛" />
+      </CharacterContainer>
+      <LoadingText>
+        {fullText.split("").map((char, index) => (
+          <span key={index}>{char}</span>
+        ))}
+      </LoadingText>
     </LoadingContainer>
   );
 }
--- src/Pages/MainFeedPage.jsx
+++ src/Pages/MainFeedPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import tokenValidAPI from "../API/tokenValidAPI";
 import Header from "../Components/Commons/Header/Header";
@@ -6,14 +6,25 @@ import Footer from "../Components/Commons/Footer";
 // import SwitchMode from "../Components/Main/SwitchMode";
 import PostList from "../Components/Main/PostList";
 import PostDetailModal from "../Components/Main/PostDeatilModal/PostDetailModal";
+import Loading from "../Components/Commons/Loading";
 import { useRecoilValue } from "recoil";
 import { isTouchFeed } from "../Store/Store";
 
 function MainFeedPage() {
   const [tokenValid, setTokenValid] = useState(true);
+  const [isLoading, setIsLoading] = useState(true); //로딩 상태 관리
   const isVisible = useRecoilValue(isTouchFeed);
 
-  tokenValidAPI(setTokenValid);
+  useEffect(() => {
+    setIsLoading(true); // API 호출 시작 전에 로딩 상태를 true로 설정
+    tokenValidAPI(setTokenValid).finally(() => {
+      setIsLoading(false); // API 호출이 끝나면 로딩 상태를 false로 설정
+    });
+  }, []);
+
+  if (isLoading) {
+    return <Loading />; // 로딩 중일 때 <Loading/> 렌더링
+  }
 
   return tokenValid ? (
     <>
