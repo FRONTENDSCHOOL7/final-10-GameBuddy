755732f8556e762f4b8fc719753c8209785f53c1
GedFlow <ptj1327@naver.com>
2023-10-25 14:57:20 +0900

    [Fix]-image-compressor.js라이브러리를 사용, 프로필 사진으로 용량이 큰 이미지 파일을 사용했을 때 회원가입이 안되는 현상 수정

--- src/Components/SignUp/SetProfile.jsx
+++ src/Components/SignUp/SetProfile.jsx
@@ -1,71 +1,9 @@
 import React, {useState, useEffect} from 'react'
 import { useNavigate } from 'react-router-dom';
-import styled from "styled-components";
+import * as S from "./SignUpStyle";
+import ImageCompressor from 'image-compressor.js';
 import DefaultImage from '../../assets/image/char_inactive.png';
-import ImageChangeBtn from '../../assets/image/img-change.png'
 import signUpAPI from '../../API/signUpAPI';
-
-const Container = styled.div`
-  background-color: #ffffff;
-  width: 100%;
-  height: 100vh;
-  text-align: center;
-  position:relative;
-  font-size:12px;
-`
-const SetProfileForm = styled.form`
-  margin: 0 auto;
-  width: 86%;
-  position: relative;
-  top:20%;
-  color: #767676;
-`
-const ProfileSettingLogo = styled.p`
-  margin-bottom: 36px;
-  font-size: 25px;
-  font-weight:bold;
-  color: #000000;
-`
-const ImageChangeButton = styled.input`
-  background-image: ${ImageChangeBtn};
-`
-const PTag = styled.p`
-    margin:14px 0 6px 0;
-    text-align:left;
-`
-const InputTag = styled.input`
-  width: 100%;
-  height: 23px;
-  border-width: 0 0 1px;
-  padding: 0 0 1px 0;
-  font-size: 20px;
-  &:focus {
-    outline: none;
-    border-color: #DBDBDB;
-  }
-  &::placeholder {
-    color: #DBDBDB;
-    font-size: 14px;
-  }
-`
-const SubmitBtn = styled.button`
-width: 100%;
-height: 44px;
-margin-top: 30px;
-border-radius: 44px;
-border: none;
-background-color: ${props => props.disabled ? '#8EA1E1' : '#5865f2'};
-/* background-color: #8EA1E1; */
-/* background-color: #5865f2; */
-color: #ffffff;
-`
-const Warning = styled.p`
-  color: #ff0000;
-  text-align:left;
-  font-size: 12px;
-  margin-top: 4px;
-`
-
 
 function SetProfile({email, password}) {
   const [userName, setUserName] = useState('');
@@ -114,11 +52,18 @@ function SetProfile({email, password}) {
   const handleImageChange = (e) => {
     const file = e.target.files[0];
     if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setSelectedImage(reader.result);
-      };
-      reader.readAsDataURL(file);
+      new ImageCompressor(file, {
+        maxWidth: 100,
+        maxHeight: 100,
+        quality: 1, // 압축 품질 조정. 필요에 따라 이 값을 조절하세요.
+        success(result) {
+          const imageUrl = URL.createObjectURL(result);
+          setSelectedImage(imageUrl)
+        },
+        error(err) {
+          console.log(err.message);
+        },
+      });
     }
   };
   const handleImageClick = () => {
@@ -126,37 +71,35 @@ function SetProfile({email, password}) {
   };
 
   return (
-    <Container>
-      <SetProfileForm onSubmit={onSubmitHandler}>
-        <ProfileSettingLogo>프로필 설정</ProfileSettingLogo>
+    <S.Container>
+      <S.SetProfileForm onSubmit={onSubmitHandler}>
+        <S.ProfileSettingLogo>프로필 설정</S.ProfileSettingLogo>
 
-        <img
+        <S.ProfileImage
          src={selectedImage || DefaultImage} 
          alt="프로필 이미지" 
          onClick={handleImageClick}
-         style={{cursor: 'pointer'}}
          />
-        <input
+        <S.ProfileImageChange
           type='file'
           onChange={handleImageChange}
           ref={fileInputRef}
           accept="image/jpeg, image/png"
-          style={{display:'none'}}
         />
 
-        <PTag>사용자이름</PTag>
-        <InputTag type="text" onChange={onChangeUserName} placeholder='2~10자 이내여야 합니다.' />
-        <Warning style={isSignUp === 2 ? {display:"block"} : {display:"none"}}>*유저이름은 2~10자 이내여야 합니다.</Warning>
+        <S.PTag>사용자이름</S.PTag>
+        <S.InputTag type="text" onChange={onChangeUserName} placeholder='2~10자 이내여야 합니다.' />
+        <S.Warning style={isSignUp === 2 ? {display:"block"} : {display:"none"}}>*유저이름은 2~10자 이내여야 합니다.</S.Warning>
         
-        <PTag>계정ID</PTag>
-        <InputTag type="text" onChange={onChangeAccountName} placeholder='영문, 숫자, 마침표, 밑줄만 사용 가능합니다. ' />
-        <Warning style={isSignUp === 1 ? {display:"block"} : {display:"none"}}>*영문, 숫자, 밑줄 및 마침표만 사용할 수 있습니다.</Warning>
+        <S.PTag>계정ID</S.PTag>
+        <S.InputTag type="text" onChange={onChangeAccountName} placeholder='영문, 숫자, 마침표, 밑줄만 사용 가능합니다. ' />
+        <S.Warning style={isSignUp === 1 ? {display:"block"} : {display:"none"}}>*영문, 숫자, 밑줄 및 마침표만 사용할 수 있습니다.</S.Warning>
         
-        <PTag>소개</PTag>
-        <InputTag type="text" onChange={onChangeIntro} placeholder='자신을 소개해주세요!' />
-        <SubmitBtn type='submit'>Game Buddy 시작하기!</SubmitBtn>
-      </SetProfileForm>
-    </Container>
+        <S.PTag>소개</S.PTag>
+        <S.InputTag type="text" onChange={onChangeIntro} placeholder='자신을 소개해주세요!' />
+        <S.SubmitBtn type='submit'>Game Buddy 시작하기!</S.SubmitBtn>
+      </S.SetProfileForm>
+    </S.Container>
   )
 }
 
