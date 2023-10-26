5ba1f516afaea6307bd86bc40f187c57b8905468
GedFlow <ptj1327@naver.com>
2023-10-26 14:26:41 +0900

    [Feat]-채팅리스트 더미데이를 통해 개발 완료

---
+++ src/Components/Chat/ChattingListStyle.jsx
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+export const ChatListContainer = styled.div`
+  padding-top: 62px;
+`
+
+export const ChatListForm = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 90%;
+  padding: 16px 16px 0;
+`
+
+export const ChattingBox = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+export const Image = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+`
+
+export const Article = styled.article`
+  margin-left: 12px;
+  line-height: 25px;
+`
+
+export const UserName = styled.p`
+  color: #000;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+`
+
+export const Intro = styled.p`
+  color: #767676;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  width: 180px; /* 원하는 최대 너비로 조정 가능 */
+`
+
+export const TimeP = styled.p`
+  font-size: 12px;
+`
+
