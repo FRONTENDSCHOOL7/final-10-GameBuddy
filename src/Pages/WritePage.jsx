a162606816083c61ae7939631b0078c2aa90ac44
yeonjeong.choi <kkso10043@naver.com>
2023-10-27 23:53:41 +0900

    [Feat]-WritePage API 구현

--- src/Pages/WritePage.jsx
+++ src/Pages/WritePage.jsx
@@ -61,7 +61,7 @@ const WriteImage = styled.img`
   margin-bottom: 30px;
 `;
 
-const WriteInput = styled.div`
+const WriteFormWrapper = styled.div`
   padding: 0 32px;
 `;
 
@@ -99,12 +99,29 @@ const SubmitBtn = styled.button`
   padding: 0 32px;
 `;
 
+const Warning = styled.p`
+  color: #ff0000;
+  text-align:left;
+  font-size: 12px;
+  margin-top: 4px;
+`;
+
 export default function WritePage() {
   const [selectedImage, setSelectedImage] = useState(RecruitDefaultImage);
   // 모집 글 버튼에 해당하는 이미지로 초기화
   const [selectedBtn, setSelectedBtn] = useState(1);
   // 모집 글 버튼을 선택한 상태로 초기화
   // const [name, setName]
+
+  const [recruitGame, setRecruitGame] = useState('');
+  const [recruitPeople, setRecruitPeople] = useState('');
+  const [recruitDetail, setRecruitDetail] = useState('');
+  const [postContent, setPostContent] = useState('');
+
+  const [isGameTitleValid, setIsGameTitleValid] = useState(true);
+  const [isPeopleValid, setIsPeopleValid] = useState(true);
+  const [isDetailValid, setIsDetailValid] = useState(true);
+  const [isContentValid, setIsContentValid] = useState(true);
 
   const navigate = useNavigate();
   const fileInputRef = useRef();
@@ -120,18 +137,6 @@ export default function WritePage() {
       setSelectedImage(PostDefaultImage);
     }
   };
-
-  // 이미지 변경 핸들러
-  // const handleWriteImageChange = (e) => {
-  //   const file = e.target.files[0];
-  //   if (file) {
-  //     const reader = new FileReader();
-  //     reader.onloadend = () => {
-  //       setSelectedImage(reader.result);
-  //     };
-  //     reader.readAsDataURL(file);
-  //   }
-  // };
 
   const handleWriteImageChange = async (e) => {
     const file = e.target.files[0];
@@ -157,7 +162,8 @@ export default function WritePage() {
       } catch (error) {
         console.log(error)
         if(error.response && error.response.data) {
-          alert('이미지 파일만 업로드가 가능합니다.');
+          // alert('지원되지 않는 파일 형식입니다. 이미지 파일을 선택해주세요.');
+          alert(`Error: ${error.response.data.message}`);
         } else {
           console.error(`Error: ${error}`);
         }
@@ -166,13 +172,182 @@ export default function WritePage() {
   };
 
 
+  // 저장하기 버튼 모집 / 소통 글
+  const handlePostSubmit = async () => {
+    let product = {};
+    if (selectedBtn === 1) { 
+      // if (!recruitGame || !recruitPeople || !recruitDetail || recruitPeople < 1 || !postContent) {
+      //   alert('필수 입력사항을 입력해주세요.');
+      //   return;
+      // }
+      product = {
+        product: {
+          itemImage: selectedImage,
+          itemName: recruitGame, 
+          price: parseInt(recruitPeople), 
+          link: recruitDetail
+          
+        }
+      };
+    } else if (selectedBtn === 2) {
+      // if (!postContent) {
+      //   alert('필수 입력사항을 입력해주세요.');
+      //   return;
+      // }
+      product = {
+        product: {
+          itemImage: selectedImage,
+          itemName: postContent
+          
+        }
+      };
+    }
+
+    // localStorage에서 토큰을 가져옵니다.
+    const token = localStorage.getItem('token');
+  
+    try {
+      const response = await axios.post('https://api.mandarin.weniv.co.kr/product', product, {
+      headers: {
+        "Authorization" : `Bearer ${token}`, 
+        "Content-type" : "application/json"
+        }
+      });
+
+      console.log(response)  
+  
+      if(response.status === 200) {
+        console.log('서버 응답: ', response);
+        alert('게시글이 성공적으로 등록되었습니다.');
+        navigate('/main');
+      }
+    } catch (error) {
+      console.error('서버 오류: ', error.response);
+      if(error.response && error.response.data) {
+        alert(`Error: ${error.response.data.message}`);
+      } else {
+        alert('게시글 등록에 실패했습니다.');
+      }
+    }
+};
+
+
+
+  // const onChangeRecruitGame = (e) => {
+  //   const recruitGame = e.target.value;
+  //   console.log("모집 게임: ", recruitGame);
+  //   if(recruitGame.length < 2 || recruitGame.length > 15) {
+  //     setIsGameTitleValid(false);
+  //   } else {
+  //     setIsGameTitleValid(true);
+  //   }
+  //   setRecruitGame(recruitGame);
+  // };
+
+  const onChangeRecruitGame = (e) => {
+    try {
+      const recruitGame = e.target.value;
+      console.log("모집 게임: ", recruitGame);
+      if(recruitGame.length < 2 || recruitGame.length > 15) {
+        setIsGameTitleValid(false);
+      } else {
+        setIsGameTitleValid(true);
+      }
+      setRecruitGame(recruitGame);
+    } catch (error) {
+      console.error('모집 게임 입력 오류: ', error);
+    }
+  };
+  
+
+  // const onChangeRecruitPeople = (e) => {
+  //   const recruitPeople = e.target.value;
+  //   console.log("모집 인원: ", recruitPeople);
+  //   if(!/^\d+$/.test(recruitPeople)) { 
+  //     setIsPeopleValid(false);
+  //   } else {
+  //     setIsPeopleValid(true);
+  //   }
+  //   setRecruitPeople(recruitPeople);
+  // };
+
+  const onChangeRecruitPeople = (e) => {
+    try {
+      const recruitPeople = e.target.value;
+      console.log("모집 인원: ", recruitPeople);
+      if(!/^\d+$/.test(recruitPeople)) {
+        setIsPeopleValid(false);
+      } else {
+        setIsPeopleValid(true);
+      }
+      setRecruitPeople(recruitPeople);
+    } catch (error) {
+      console.error('모집 인원 입력 오류: ', error);
+    }
+  };
+  
+
+  // const onChangeRecruitDetail = (e) => {
+  //   const recruitDetail = e.target.value;
+  //   console.log("모집 상세: ", recruitDetail);
+  //   if(recruitDetail === '') { 
+  //     setIsDetailValid(false);
+  //   } else {
+  //     setIsDetailValid(true);
+  //   }
+  //   setRecruitDetail(recruitDetail);
+  // };
+
+  const onChangeRecruitDetail = (e) => {
+    try {
+      const recruitDetail = e.target.value;
+      console.log("모집 상세: ", recruitDetail);
+      if(recruitDetail === '') { 
+        setIsDetailValid(false);
+      } else {
+        setIsDetailValid(true);
+      }
+      setRecruitDetail(recruitDetail);
+    } catch (error) {
+      console.error('모집 상세 입력 오류: ', error);
+    }
+  };
+
+  // const onChangePostContent = (e) => {
+  //   const postContent = e.target.value;
+  //   console.log("게시글: ", postContent);
+  //   if(postContent === '') {
+  //     setIsContentValid(false);
+  //   } else {
+  //     setIsContentValid(true);
+  //   }
+  //   setPostContent(postContent);
+  // };
+
+  const onChangePostContent = (e) => {
+    try {
+      const postContent = e.target.value;
+      console.log("게시글: ", postContent);
+      if(postContent === '') {
+        setIsContentValid(false);
+      } else {
+        setIsContentValid(true);
+      }
+      setPostContent(postContent);
+    } catch (error) {
+      console.error('게시글 입력 오류: ', error);
+    }
+  };
+  
+
+
+
+
+
+
 
   const handleWriteImageClick = () => {
     fileInputRef.current.click();
-  };
-
-  const handleClick = () => {
-    navigate('/main');
   };
 
   return (
@@ -181,7 +356,7 @@ export default function WritePage() {
 
       <ButtonImgContainer>
         <Button onClick={() => handleButtonClick(1)} selected={selectedBtn === 1}>모집글 버튼</Button>
-        <Button onClick={() => handleButtonClick(2)} selected={selectedBtn === 2}>소집글 버튼</Button>
+        <Button onClick={() => handleButtonClick(2)} selected={selectedBtn === 2}>소통글 버튼</Button>
       </ButtonImgContainer>
 
       <ImageContainer>
@@ -194,8 +369,8 @@ export default function WritePage() {
         />
       </ImageContainer>
 
-      <WriteInput>
-        {/* 모집글 작성하기 */}
+      <WriteFormWrapper>
+        {/* 모집글 recruit 작성하기 */}
         {selectedBtn === 1 && (
           <>
             <input
@@ -205,18 +380,25 @@ export default function WritePage() {
               accept="image/jpeg, image/png"
               style={{ display: 'none' }}
             />
+
             <PTag>모집 게임</PTag>
-            <InputTag type="text" placeholder='2~15자 이내여야 합니다.' />
+            <InputTag type="text" placeholder='2~15자 이내여야 합니다.' 
+              value={recruitGame} onChange={onChangeRecruitGame} />
+            <Warning style={isGameTitleValid ? {display:"none"} : {display:"block"}}>*2~15자 이내여야 합니다.</Warning>
 
             <PTag>모집 인원</PTag>
-            <InputTag type="text" placeholder='숫자만 입력 가능합니다. ' />
+            <InputTag type="text" placeholder='숫자만 입력 가능합니다. ' 
+              value={recruitPeople} onChange={onChangeRecruitPeople} />
+            <Warning style={isPeopleValid ? {display:"none"} : {display:"block"}}>*숫자만 입력 가능합니다.</Warning>
 
             <PTag>모집 상세</PTag>
-            <InputTag type="text" placeholder='포지션, 티어 등 상세 내용을 입력해주세요.' />
+            <InputTag type="text" placeholder='포지션, 티어 등 상세 내용을 입력해주세요.' 
+              value={recruitDetail} onChange={onChangeRecruitDetail} />
+            <Warning style={isDetailValid ? {display:"none"} : {display:"block"}}>*포지션, 티어 등 상세 내용을 입력해주세요.</Warning>
           </>
         )}
 
-        {/* 소통글 작성하기 */}
+        {/* 소통글 post 작성하기 */}
         {selectedBtn === 2 && (
           <>
             <input
@@ -228,11 +410,13 @@ export default function WritePage() {
             />
 
             <PTag>게시글</PTag>
-            <InputTag type="text" placeholder='게시글 내용을 입력해주세요.' />
+            <InputTag type="text" placeholder='게시글 내용을 입력해주세요.' 
+              value={postContent} onChange={onChangePostContent} />
+            <Warning style={isContentValid ? {display:"none"} : {display:"block"}}>*게시글 내용을 입력해주세요.</Warning>
           </>
         )}
-        <SubmitBtn type='submit' onClick={handleClick}>저장하기</SubmitBtn>
-      </WriteInput>
+        <SubmitBtn type='submit' onClick={handlePostSubmit}>저장하기</SubmitBtn>
+      </WriteFormWrapper>
     </WriteContainer>
   );
 }
