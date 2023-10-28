06e365878c1c9e9fc257b6e0df6950163889d165
GedFlow <ptj1327@naver.com>
2023-10-28 22:22:52 +0900

    [Refactory]-기존 API코드 axois 사용법 통일 의미불명이거나 필요없는 console.log 개선(확실하게 의미부여) 및 삭제

--- src/API/followAPI/unFollowAPI.js
+++ src/API/followAPI/unFollowAPI.js
@@ -3,21 +3,37 @@ import axios from "axios";
 async function unFollowAPI(accountName) {
     try {
         const token = localStorage.getItem("token");
-        let result = await axios({
-            method: 'delete',
-            url: `https://api.mandarin.weniv.co.kr/profile/${accountName}/unfollow`,
+        const result = await axios.delete(`https://api.mandarin.weniv.co.kr/profile/${accountName}/unfollow`, {
             headers: {
                 "Authorization": `Bearer ${token}`
-            },
+            }
         })
-        
-        console.log("언팔로우 완료")
-        console.log(result)
-        return true
+        console.log(`${accountName}님을 성공적으로 언팔로우 했습니다!`);
+        return true;
     } catch (error) {
-        console.log("언팔로우 api 에러: ", error)
-        return false
+        console.log(`${accountName}님을 언팔로우하지 못했습니다`, error);
+        return false;
     }
-};
+}
 
 export default unFollowAPI;
+
+// async function unFollowAPI(accountName) {
+//     try {
+//         const token = localStorage.getItem("token");
+//         let result = await axios({
+//             method: 'delete',
+//             url: `https://api.mandarin.weniv.co.kr/profile/${accountName}/unfollow`,
+//             headers: {
+//                 "Authorization": `Bearer ${token}`
+//             },
+//         })
+        
+//         console.log("언팔로우 완료")
+//         console.log(result)
+//         return true
+//     } catch (error) {
+//         console.log("언팔로우 api 에러: ", error)
+//         return false
+//     }
+// };
