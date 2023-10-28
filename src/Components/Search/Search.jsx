9ef3bad914f708c9bd353dcb3327da43458459b5
Hajin-Bang <hajin0619@naver.com>
2023-10-28 15:47:06 +0900

    [Feat]-검색 기능 구현(디바운스 포함)

---
+++ src/Components/Search/Search.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import * as S from "./SearchStyle";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { searchKeywordAtom, userSearchResultAtom } from "../../Store/Store";
+import searchUserAPI from "../../API/searchUserAPI";
+import { useNavigate, useParams } from "react-router-dom";
+import { ReactComponent as GoBackIcon } from "../../assets/image/icon-goback.svg";
+
+function Search() {
+  const searchKeyword = useRecoilValue(searchKeywordAtom);
+  const setSearchKeyword = useSetRecoilState(searchKeywordAtom);
+  const [searchResult, setSearchResult] = useRecoilState(userSearchResultAtom);
+
+  // 디바운스를 위한 입력값 타이머
+  const [timer, setTimer] = useState(null);
+
+  // input에 검색어를 입력하면 호출되는 핸들러
+  const handleInputChange = (e) => {
+    setSearchKeyword(e.target.value); // 검색 키워드 상태 업데이트
+
+    if (timer) {
+      clearTimeout(timer); // 타이머 초기화
+    }
+
+    // 사용자가 키워드를 입력한지 700ms 후에 api를 호출
+    const newTimer = setTimeout(async () => {
+      if (e.target.value) {
+        const results = await searchUserAPI(e.target.value);
+        setSearchResult(results); // 검색 결과 상태 업데이트
+      }
+    }, 600);
+    setTimer(newTimer); // 타이머 상태 업데이트
+  };
+
+  // goBack 버튼: 이전 페이지로 이동
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <>
+      <S.HeaderContainer>
+        <S.StyledIconButton onClick={goBack}>
+          <GoBackIcon />
+        </S.StyledIconButton>
+        <S.Input
+          id="search"
+          placeholder="계정 검색"
+          onChange={handleInputChange}
+        />
+      </S.HeaderContainer>
+      <S.SearchUserContainer>
+        {searchResult.map((data, id) => {
+          return (
+            <S.SearchResultForm key={id}>
+              <S.SearchUserBox
+                onClick={() => navigate(`/profile/${data.accountname}`)}>
+                <S.Image src={data.image} />
+                <S.Article>
+                  <S.UserName>{data.username}</S.UserName>
+                  <S.Intro>@{data.accountname}</S.Intro>
+                </S.Article>
+              </S.SearchUserBox>
+            </S.SearchResultForm>
+          );
+        })}
+      </S.SearchUserContainer>
+    </>
+  );
+}
+
+export default Search;
---
+++ src/Components/Search/SearchStyle.jsx
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 13px 20px 12px 12px;
+  background-color: #ffff;
+  border-bottom: 1px solid #dbdbdb;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 1000;
+
+  box-sizing: border-box;
+`;
+
+export const StyledIconButton = styled.button`
+  border: none;
+  background: none;
+  cursor: pointer;
+  padding: 5px;
+`;
+
+export const Input = styled.input`
+  flex: 1;
+  padding: 8px;
+  background-color: #f2f2f2;
+  border: 1px solid #f2f2f2;
+  border-radius: 32px;
+`;
+
+export const SearchUserContainer = styled.div`
+  padding-top: 62px;
+`;
+
+export const SearchResultForm = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 90%;
+  padding: 16px 16px 0;
+`;
+
+export const SearchUserBox = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Image = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+`;
+
+export const Article = styled.article`
+  margin-left: 12px;
+  line-height: 25px;
+`;
+
+export const UserName = styled.p`
+  color: #000;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+`;
+
+export const Intro = styled.p`
+  color: #767676;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  width: 180px; /* 원하는 최대 너비로 조정 가능 */
+`;
