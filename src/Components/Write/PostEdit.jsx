b40fe0e9fde276c81d59e522e44cc789e5213acc
yeonjeong.choi <kkso10043@naver.com>
2023-11-03 17:21:31 +0900

    [Style]-게시글 수정 페이지 디자인 수정

--- src/Components/Write/PostEdit.jsx
+++ src/Components/Write/PostEdit.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import * as S from "./WriteStyle";
+import * as S from "./PostEditStyle";
 import Header from "../Commons/Header/Header";
 import DefaultImage from "../../assets/image/WriteDefault.svg";
 import { uploadImageAtom } from "../../Store/Store";
@@ -8,21 +8,19 @@ import { useRecoilState } from "recoil";
 import getPostAPI from "../../API/getPostAPI";
 import postEditAPI from "../../API/postEditAPI";
 import uploadImageAPI from "../../API/uploadImageAPI";
-import { useParams } from 'react-router-dom';
 import { useNavigate, useLocation } from "react-router-dom";
 
 function PostEdit() {
   const resetRecoilState = useResetRecoilState(uploadImageAtom);
   const [postContent, setPostContent] = useState("");
   const [postImage, setPostImage] = useState("");
-  // const { post_id } = useParams();
   const [isContentValid, setIsContentValid] = useState(true);
   const [uploadImage, setUploadImage] = useRecoilState(uploadImageAtom);
   const fileInputRef = useRef();
   const navigate = useNavigate();
 
-  const { state } = useLocation(); // useLocation을 사용하여 state를 가져옴
-  const post_id = state.post_id; // state에서 post_id를 가져옴 
+  const { state } = useLocation();
+  const post_id = state.post_id;
 
 
   useEffect(() => {
@@ -78,41 +76,45 @@ function PostEdit() {
   return (
     <S.WriteContainer>
       <Header type="profileMod" />
-      <S.ImageContainer>
-      <h5 style={{paddingTop: '114px'}}>이미지 등록</h5>
-        <S.WriteImage
-          src={uploadImage}
-          alt="게시글 이미지"
-          onClick={handleWriteImageClick}
-          onError={(e) => { e.target.onerror = null; e.target.src=DefaultImage }}
-        />
-      </S.ImageContainer>
-  
-      <S.WriteFormWrapper>
-        <input
-          type="file"
-          onChange={handleWriteImageChange}
-          ref={fileInputRef}
-          accept="image/jpeg, image/png"
-          style={{ display: "none" }}
-        />
-  
-        <S.PTag>게시글 수정</S.PTag>
-        <S.InputTag
-          type="text"
-          placeholder={postContent}
-          value={postContent}
-          onChange={onChangePostContent}
-        />
-        <S.Warning
-          style={isContentValid ? { display: "none" } : { display: "block" }}
-        >
-          *게시글 내용을 입력해주세요.
-        </S.Warning>
-        <S.SubmitBtn type="submit" onClick={handlePostSubmit}>
-          수정하기
-        </S.SubmitBtn>
-      </S.WriteFormWrapper>
+      <S.Divdiv>
+        <S.ContentContainer>
+          <S.ImageContainer>
+          <h5>이미지 등록</h5>
+            <S.WriteImage
+              src={uploadImage}
+              alt="게시글 이미지"
+              onClick={handleWriteImageClick}
+              onError={(e) => { e.target.onerror = null; e.target.src=DefaultImage }}
+            />
+          </S.ImageContainer>
+      
+          <S.WriteFormWrapper>
+            <input
+              type="file"
+              onChange={handleWriteImageChange}
+              ref={fileInputRef}
+              accept="image/jpeg, image/png"
+              style={{ display: "none" }}
+            />
+      
+            <S.PTag>게시글 수정</S.PTag>
+            <S.TextAreaTag
+              type="text"
+              placeholder={postContent}
+              value={postContent}
+              onChange={onChangePostContent}
+            />
+            <S.Warning
+              style={isContentValid ? { display: "none" } : { display: "block" }}
+            >
+              *게시글 내용을 입력해주세요.
+            </S.Warning>
+            <S.SubmitBtn type="submit" onClick={handlePostSubmit}>
+              수정하기
+            </S.SubmitBtn>
+          </S.WriteFormWrapper>
+        </S.ContentContainer>
+      </S.Divdiv>
     </S.WriteContainer>
   );  
 }
